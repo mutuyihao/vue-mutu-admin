329e929e423df30ffdef628ed2e6a67b34cf768d
mutuyihao <96763582+mutuyihao@users.noreply.github.com>
2023-03-21 22:51:57 +0800

    fix:remove a useless icon

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,18 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import type { RouteRecordRaw } from 'vue-router'
-const routes: any[] = [
-    {
-        name: 'login',
-        path: '/login',
-        component: () => import('@/views/login/login.vue'),
-        meta:{
-            title:"登录"
-        }
-    }
-]
-const router = createRouter({
-    history:createWebHistory(),
-    routes,
-})
-export default router
+
+
+
+
 
